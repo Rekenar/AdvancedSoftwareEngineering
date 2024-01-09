9ed0aa5cbd279132642b01ddd8401064359ede58
vrebol <vid.rebol11@gmail.com>
2024-01-09 17:17:12 +0100

    Cities dataloader classes

--- backend/src/main/java/com/example/backend/repositories/AlmostCityRepo.java
+++ backend/src/main/java/com/example/backend/repositories/AlmostCityRepo.java
@@ -1,18 +1,24 @@
 package com.example.backend.repositories;
 
 import com.example.backend.models.AlmostCityEntity;
+import com.example.backend.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AlmostCityRepo extends JpaRepository<AlmostCityEntity, Long> {
+
+    Optional<AlmostCityEntity> findByCity(String city);
 
     @Query(value = "SELECT * FROM almost_game_capitals ORDER BY RAND() LIMIT 10", nativeQuery = true)
     List<AlmostCityEntity> sampleCities();
 
     @Query(value = "SELECT * FROM almost_game_capitals WHERE capital=true ORDER BY RAND() LIMIT 10", nativeQuery = true)
     List<AlmostCityEntity> sampleCapitals();
+
+
 }
