ef73f91646cc4d903ea71c974d2b08ceb185423e
Lukas Kofler <lakitwo@icloud.com>
2024-01-19 10:10:46 +0100

    Setup chrome for running selenium tests in GitHub Automations

--- backend/src/main/java/com/example/backend/libraries/WebDriverLibrary.java
+++ backend/src/main/java/com/example/backend/libraries/WebDriverLibrary.java
@@ -3,6 +3,7 @@ package com.example.backend.libraries;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,14 @@ public class WebDriverLibrary {
     @Bean
     public WebDriver getChromeDriver() {
         WebDriverManager.chromedriver().setup();
+
+        // Setting the options to work in GitHub Actions CI environment
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         return new ChromeDriver();
     }
 
