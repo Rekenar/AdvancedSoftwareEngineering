d882ef95f6582776d5e50129eaab546abb508a0e
Lukas Kofler <lakitwo@icloud.com>
2024-01-19 09:52:55 +0100

    Fix Selenium Tests for Login Expect Unauthorized

--- backend/src/main/java/com/example/backend/dtos/UserDetailsDTO.java
+++ backend/src/main/java/com/example/backend/dtos/UserDetailsDTO.java
@@ -5,12 +5,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
 public class UserDetailsDTO implements UserDetails {
