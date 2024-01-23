a4cfbcf91678f0d2149f1135fe42b0ebba7e3648
Lukas Kofler <lakitwo@icloud.com>
2024-01-23 18:59:41 +0100

    replace hardcoded Game Names

--- frontend-new/src/app/game.enum.ts
+++ frontend-new/src/app/game.enum.ts
@@ -1,9 +1,9 @@
 export enum Game {
   NoGame = "No Game",
-  Game1 = "Almost Game",
-  Game2 = "Movie Guessr",
+  Game1 = "Almost-Game",
+  Game2 = "Movie-Guessr",
   Game3 = "Snake",
-  Game4 = "Typing Game",
+  Game4 = "Typing-Game",
   Game5 = "Asteroid",
   Scoreboard = "Scoreboard"
 }
